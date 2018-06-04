11ceababc7e6ce8d4d4125b40d790c99f8c9aea0
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-06-04 17:09:02 +0800

    modify test and code with di and pass the test

--- src/test/java/com/odde/securetoken/AuthenticationServiceTest.java
+++ src/test/java/com/odde/securetoken/AuthenticationServiceTest.java
@@ -3,16 +3,34 @@ package com.odde.securetoken;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class AuthenticationServiceTest {
 
+    private static final String PASSWORD = "91";
+    private static final String TOKEN = "607934";
+    ProfileDao stubProfileDao = mock(ProfileDao.class);
+    RsaTokenDao stubRsaTokenDao = mock(RsaTokenDao.class);
+    AuthenticationService target = new AuthenticationService(stubProfileDao, stubRsaTokenDao);
+
     @Test
     public void is_valid_test() {
-        AuthenticationService target = new AuthenticationService();
+        givenAccountPassword(PASSWORD);
+        givenRsaToken(TOKEN);
 
-        boolean actual = target.isValid("joey", "91000000");
+        boolean actual = target.isValid("joey", PASSWORD + TOKEN);
 
         assertTrue(actual);
+    }
+
+    private void givenRsaToken(String token) {
+        when(stubRsaTokenDao.getRandom(anyString())).thenReturn(token);
+    }
+
+    private void givenAccountPassword(String password) {
+        when(stubProfileDao.getPassword(anyString())).thenReturn(password);
     }
 
 }
