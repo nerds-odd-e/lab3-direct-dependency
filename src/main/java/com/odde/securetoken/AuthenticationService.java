f19cff1e43599ccba1a97b348451d5b5ef894c38
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-06-05 11:19:42 +0800

    Revert "modify test and code with di and pass the test"
    This reverts commit 11ceababc7e6ce8d4d4125b40d790c99f8c9aea0.

--- src/main/java/com/odde/securetoken/AuthenticationService.java
+++ src/main/java/com/odde/securetoken/AuthenticationService.java
@@ -1,20 +1,14 @@
 package com.odde.securetoken;
 
 public class AuthenticationService {
-    private final ProfileDao profileDao;
-    private final RsaTokenDao rsaTokenDao;
-
-    public AuthenticationService(ProfileDao profileDao, RsaTokenDao rsaTokenDao) {
-        this.profileDao = profileDao;
-        this.rsaTokenDao = rsaTokenDao;
-    }
-
     public boolean isValid(String account, String password) {
         // 根據 account 取得自訂密碼
+        ProfileDao profileDao = new ProfileDao();
         String passwordFromDao = profileDao.getPassword(account);
 
         // 根據 account 取得 RSA token 目前的亂數
-        String randomCode = rsaTokenDao.getRandom(account);
+        RsaTokenDao rsaToken = new RsaTokenDao();
+        String randomCode = rsaToken.getRandom(account);
 
         // 驗證傳入的 password 是否等於自訂密碼 + RSA token亂數
         String validPassword = passwordFromDao + randomCode;
--- src/test/java/com/odde/securetoken/AuthenticationServiceTest.java
+++ src/test/java/com/odde/securetoken/AuthenticationServiceTest.java
@@ -3,34 +3,16 @@ package com.odde.securetoken;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class AuthenticationServiceTest {
 
-    private static final String PASSWORD = "91";
-    private static final String TOKEN = "607934";
-    ProfileDao stubProfileDao = mock(ProfileDao.class);
-    RsaTokenDao stubRsaTokenDao = mock(RsaTokenDao.class);
-    AuthenticationService target = new AuthenticationService(stubProfileDao, stubRsaTokenDao);
-
     @Test
     public void is_valid_test() {
-        givenAccountPassword(PASSWORD);
-        givenRsaToken(TOKEN);
+        AuthenticationService target = new AuthenticationService();
 
-        boolean actual = target.isValid("joey", PASSWORD + TOKEN);
+        boolean actual = target.isValid("joey", "91000000");
 
         assertTrue(actual);
-    }
-
-    private void givenRsaToken(String token) {
-        when(stubRsaTokenDao.getRandom(anyString())).thenReturn(token);
-    }
-
-    private void givenAccountPassword(String password) {
-        when(stubProfileDao.getPassword(anyString())).thenReturn(password);
     }
 
 }
